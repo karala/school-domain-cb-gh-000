81af6421943ce5dc244805108ac814d7eb872c45
Shubham Karala <shubham@ramanujan.du.ac.in>
2019-10-30 18:01:30 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -17,10 +17,9 @@ class School
 
   def sort
     sorted = {}
-      roster.each do |grade, students|
-        sorted[grade] = students.sort
-      end
-      sorted
+    roster.each do |grade, students|
+      sorted[grade] = students.sort
     end
+      sorted
   end
 end
