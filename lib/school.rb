65f8a0af2e32c9f90963de1abec541bf93b02bac
Shubham Karala <shubham@ramanujan.du.ac.in>
2019-10-30 17:47:23 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,16 @@
-# code here!
+class School
+  attr_accessor :lenght, :student
+
+  def initialize(name)
+    @name = name
+  end
+
+  def roster
+    @length = Hash.new
+  end
+
+  def add_student
+
+  end
+
+end
